620fe7107a532a6f55f27f49237b93a4ec71c1f4
javidrandhir <davidbilla12378@gmail.com>
2022-09-07 17:24:12 +0530

    initial

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {useRouter} from 'next/router'
-import Image from 'next/Image'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const Header = () => {
