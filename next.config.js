55efb33304497cfc7ad7d3fb640731ea23b24af3
javidrandhir <davidbilla12378@gmail.com>
2022-09-11 12:45:58 +0530

    initial

--- next.config.js
+++ next.config.js
@@ -6,9 +6,15 @@
 //     ignoreDuringBuilds: true,
 //   },
 // };
+const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable:process.env.NODE_ENV === 'development'
+  register:true,
+  skipWaiting:true,
+  runtimeCaching,
+  fallbacks:{
+    document:'/_offline'
+  }
 })
 module.exports = withPWA({
   images: {
--- pages/_offline.js
+++ pages/_offline.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import Head from 'next/head'
 
-const _offline = () => {
-  return (
-    <div>_offline</div>
-  )
-}
+const Fallback = () => (
+  <>
+    <Head>
+      <title>next-pwa example</title>
+    </Head>
+    <h1>This is offline fallback page</h1>
+    <h2>When offline, any page route will fallback to this page</h2>
+  </>
+)
 
-export default _offline
+export default Fallback
